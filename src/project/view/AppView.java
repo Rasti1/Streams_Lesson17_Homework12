20b4e785dcfe84875a220e82a6149109701254ed
Rasti1 <rostrostov1999@gmail.com>
2024-02-26 13:19:29 +0200

    Fix methods

--- src/project/view/AppView.java
+++ src/project/view/AppView.java
@@ -11,9 +11,8 @@ public class AppView {
         AtomicInteger counter = new AtomicInteger(1);
 
         System.out.println("List of users: ");
-        users.forEach(user -> {
-            System.out.println(counter.getAndIncrement() + ") " + user.toString());
-        });
+        users.forEach(user -> System.out.println(counter.getAndIncrement()
+                + ") " + user.toString()));
 
     }
     public void filterUsers(Stream<User> users){
@@ -22,9 +21,8 @@ public class AppView {
 
         System.out.println("Filtered list: ");
         users.filter(user -> user.getEmail().contains("123"))
-                .forEach(user -> {
-                    System.out.println(counter.getAndIncrement() + ") " + user.toString());
-                });
+                .forEach(user -> System.out.println(counter.getAndIncrement()
+                        + ") " + user.toString()));
 
     }
 }
